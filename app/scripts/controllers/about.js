6b7f6ee768a9789dc0e5ba744c7b6d79d7bb7091
joeunha <imjoeunha@gmail.com>
2016-04-13 23:09:23 +0900

    First commit $ yo angular prato

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name pratoApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the pratoApp
+ */
+angular.module('pratoApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name pratoApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the pratoApp
+ */
+angular.module('pratoApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
